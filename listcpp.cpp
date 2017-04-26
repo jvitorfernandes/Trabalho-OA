d100314ae00e8200863722a0f33149217089f936
Jader Martins <jadermcs94@gmail.com>
2017-04-26 20:46:27 -0300

    removing extra spaces

--- listcpp.cpp
+++ listcpp.cpp
@@ -10,13 +10,13 @@ int main(int argc, char *argv[])
     char filename[256];
     cout << "Enter the name of the file: " << flush;
     cin >> filename;
-    file . open(filename, ios::in);
-    file . unsetf(ios::skipws);
+    file.open(filename, ios::in);
+    file.unsetf(ios::skipws);
     while (1) {
         file >> ch;
         if (file.fail()) break;
         cout << ch;
     }
-    file . close();
+    file.close();
     return 0;
 }
