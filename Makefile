ea2e2965a99a38d4291c6e0ff556f4edb1fb545e
Diógenes Oliveira <diogenes1oliveira+github@gmail.com>
2017-06-14 20:34:51 -0300

    Fixed the build files and switched from GTest to Catch

--- Makefile
+++ Makefile
@@ -8,18 +8,18 @@ INCLUDE = include
 TEST = test
 
 MAIN_TARGET = menu-main
-SRC_FILES = libfat menu-main
+SRC_FILES = libfat disk menu-main
+SRC_LIB_FILES = libfat disk
 HEADERS_FILES = libfat.h
-TEST_FILES = test-drive-simple
-
-# Flags to compile the tests
-# If gtest is installed in a different location, add the linking
-# and include directories here
-TEST_FLAGS = -lgtest -lpthread
+TEST_FILES = test-fat-simple test-disk
 
 # Generate the full path for the cpp files (usually src/srcName.cpp)
 _SRC_PATHS = $(addprefix $(SRC)/, $(SRC_FILES))
 SRC_PATHS = $(addsuffix .cpp, $(_SRC_PATHS))
+
+# Generate the full path for the cpp files (usually src/srcName.cpp)
+_SRC_LIB_PATHS = $(addprefix $(SRC)/, $(SRC_LIB_FILES))
+SRC_LIB_PATHS = $(addsuffix .cpp, $(_SRC_LIB_PATHS))
 
 # Generate the full path for the .h files (usually include/headerName)
 HEADERS_PATHS = $(addprefix $(INCLUDE)/, $(HEADERS_FILES))
@@ -46,14 +46,14 @@ $(OBJ)/%.o: $(SRC)/%.cpp $(HEADERS_PATHS)
 	$(CC) -c -o $@ $< $(INCLUDE_FLAGS)
 
 run-tests: build-tests
-	$(foreach var,$(TEST_BIN_PATHS),./$(var) && ) :
+	@$(foreach var,$(TEST_BIN_PATHS), echo "RUNNING " $(var); ./$(var); ) :
 
 build-tests: $(TEST_BIN_PATHS)
 
 # Build the tests (all the binaries in TEST_BIN_PATHS)
 $(TEST)/$(BIN)/%: $(TEST)/%.cpp $(HEADERS_PATHS) $(SRC_PATHS) $(OBJ_PATHS)
 	mkdir -p $(TEST)/$(BIN)
-	$(CC) -o $@ $< $(INCLUDE_FLAGS) $(TEST_FLAGS)
+	$(CC) -o $@ $< $(SRC_LIB_PATHS) $(INCLUDE_FLAGS) $(TEST_FLAGS)
 
 .PHONY: clean
 
--- test/test-disk.cpp
+++ test/test-disk.cpp
@@ -1,42 +1,48 @@
-#include "gtest/gtest.h"
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
 #include "disk.h"
 
-TEST(FromPhysical, SimpleConversion) {
+TEST_CASE("FromPhysical, SimpleConversion") {
     Disk d(10, 5, 60);
 
-    EXPECT_EQ(d.from_physical(1, 0, 0), 300);
-    EXPECT_EQ(d.from_physical(0, 1, 0), 60);
-    EXPECT_EQ(d.from_physical(0, 0, 20), 20);
+    REQUIRE(d.from_physical(1, 0, 0) == 300);
+    REQUIRE(d.from_physical(0, 1, 0) == 60);
+    REQUIRE(d.from_physical(0, 0, 20) == 20);
 
-    EXPECT_EQ(d.from_physical(9, 4, 59), 10*5*60-1);
+    REQUIRE(d.from_physical(9, 4, 59) == 10*5*60-1);
 }
 
-TEST(ToPhysical, SimpleConversion) {
+TEST_CASE("ToPhysical, SimpleConversion") {
     Disk d(10, 5, 60);
     int cylinder, track, sector;
 
     d.to_physical(300, cylinder, track, sector);
-    EXPECT_TRUE(cylinder == 1 && track == 0 && sector == 0);
+    REQUIRE(cylinder == 1);
+    REQUIRE(track == 0);
+    REQUIRE(sector == 0);
 
     d.to_physical(60, cylinder, track, sector);
-    EXPECT_TRUE(cylinder == 0 && track == 1 && sector == 0);
+    REQUIRE(cylinder == 0);
+    REQUIRE(track == 1);
+    REQUIRE(sector == 0);
 
     d.to_physical(20, cylinder, track, sector);
-    EXPECT_TRUE(cylinder == 0 && track == 0 && sector == 20);
+    REQUIRE(cylinder == 0);
+    REQUIRE(track == 0);
+    REQUIRE(sector == 20);
 
     d.to_physical(10*5*60-1, cylinder, track, sector);
-    EXPECT_TRUE(cylinder == 9 && track == 4 && sector == 59);
+    REQUIRE(cylinder == 9);
+    REQUIRE(track == 4);
+    REQUIRE(sector == 59);
 }
 
-TEST(FindFreeSector, EmptyDisk) {
+TEST_CASE("FindFreeSector, EmptyDisk") {
     Disk d(10, 5, 60);
     
-    EXPECT_EQ(d.find_free_sector(0), 0);
-    EXPECT_EQ(d.find_free_sector(40), 40);
-    EXPECT_EQ(d.find_free_sector(39, 4), 40);
-}
-
-int main(int argc, char **argv) {
-	::testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
+    REQUIRE(d.find_free_sector(0) == 0);
+    REQUIRE(d.find_free_sector(40) == 40);
+    REQUIRE(d.find_free_sector(39, 4) == 40);
 }
